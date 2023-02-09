a99b19533f344ccc09d2c2677e1c9b62868dbae9
awnishd <121890475+awnishd@users.noreply.github.com>
2023-02-09 20:10:14 +0530

    Update AccountProvider.jsx

--- src/Context/AccountProvider.jsx
+++ src/Context/AccountProvider.jsx
@@ -13,7 +13,7 @@ const AccountProvider = ({ children }) => {
     const [newMessageFlag, setNewMessageFlag] = useState(false);
 
     useEffect(() => {
-        socket.current = io('ws://localhost:9000');
+        socket.current = io('ws://whatsappsocket-vrl2.onrender.com');
     }, [])
 
     return (
