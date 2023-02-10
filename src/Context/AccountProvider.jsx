5f53a136d25aaf32221cdcd8f60f1207c05939a5
awnishd <121890475+awnishd@users.noreply.github.com>
2023-02-10 17:39:04 +0530

    Update AccountProvider.jsx

--- src/Context/AccountProvider.jsx
+++ src/Context/AccountProvider.jsx
@@ -11,7 +11,7 @@ const AccountProvider = ({ children }) => {
     const [person, setPerson] = useState({});
     const socket = useRef();
     const [newMessageFlag, setNewMessageFlag] = useState(false);
-    const ENDPOINT="https://whataspp.onrender.com";
+    const ENDPOINT="https://whatsappsocket-vrl2.onrender.com";
 
     useEffect(() => {
         socket.current = io(ENDPOINT,{ transports: ['websocket'] });
